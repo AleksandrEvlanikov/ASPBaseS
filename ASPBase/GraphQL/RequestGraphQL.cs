d4ff86523e26c7701b9fdf5847420e7f27ce56da
Aleksandr <aleksandr.evlanicov@gmail.com>
2024-01-29 22:17:20 +0300

    GraphQL

---
+++ ASPBase/GraphQL/RequestGraphQL.cs
@@ -0,0 +1,24 @@
+﻿using ASPBase.Models;
+using ASPBase.Services;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace ASPBase.GraphQL
+{
+    public class RequestGraphQL
+    {
+        private IProductRepository _productRepository;
+        private IMemoryCache _memoryCache;
+
+        public RequestGraphQL(IProductRepository productRepository, IMemoryCache memoryCache)
+        {
+            _productRepository = productRepository;
+            _memoryCache = memoryCache;
+        }
+
+        public IEnumerable<Product> AllProductGraphQL()
+        {
+            return _productRepository.GetAll();
+        }
+    }
+
+}
